83b2c18b657c80323da5f089e4c216dbd48da441
zachmokahn <zachmokahn@gmail.com>
2013-10-30 14:05:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootBook::Application.config.secret_token = '200c93957fd57d00fafa9cbee22f651836f4ffb165653d8c8934df1dd2881713da943858bd7a9d11e2d9b270d2957c605799d5296495166b15ccf6f029e07977'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BootBook::Application.config.session_store :cookie_store, key: '_BootBook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BootBook::Application.config.session_store :active_record_store
