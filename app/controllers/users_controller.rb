9951824c3f6aa1a4e1dbc178328d2d1856962392
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 09:49:42 -0500

    Login/Logout functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def login
+    if @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @error = "We were unable to find that email and password combination."
+      render 'welcome/index'
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to :root
+  end
+
   def create
     @user = User.new(params[:user])
     if @user.save
