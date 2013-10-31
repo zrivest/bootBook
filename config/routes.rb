df06a1dd20b26f3ca731dd7b40f5ab691e39d42e
zrivest <zrivest@gmail.com>
2013-10-31 14:31:18 -0500

    micro challange

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 BootBook::Application.routes.draw do
 
-  
-  resources :users
-  
-  get '/logout' => 'users#logout'
-  post '/login' => 'users#login'
-  root to: 'welcome#index'
+
+  # resources :users
+
+  get 'users' => 'users#index'
+  post 'users' => 'users#create'
+  get 'users/new', to: 'users#new', as: "new_user"
+  get 'users/:id/edit', to: 'users#edit', as: "edit_user"
+  get 'users/:id', to: 'users#show', as: "user"
+  put 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+
+  # get '/logout' => 'users#logout'
+  # post '/login' => 'users#login'
+  # root to: 'welcome#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
