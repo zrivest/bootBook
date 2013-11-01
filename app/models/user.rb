59ed495e8d1def9c2b30231e32d85a7eaa11b775
Joe Levering <joseph.w.levering@gmail.com>
2013-11-01 17:53:59 -0500

    Added socialization gem and created model/migration for like ('posivibes') functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
   validates :cohort_id, :presence => true
   validates :interest, :presence => true
 
+  act_as_liker
+
   def accept_friend(friend_id)
     request = self.pending_friendships.find_by_pending_friend_id(friend_id)
     request.approve!
